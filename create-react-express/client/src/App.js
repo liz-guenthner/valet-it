cc38084780a8bb7c74f5e38a8ebd533854323b32
Anthony Gates <setag9697@yahoo.com>
2019-09-12 20:45:13 -0500

    pickupbutton

--- create-react-express/client/src/App.js
+++ create-react-express/client/src/App.js
@@ -5,12 +5,16 @@ import LogoButton from "../src/components/logoButton";
 import LoginCard from "../src/components/loginCard";
 import ValetDriverButton from "../src/components/valetdriverbutton";
 import GuestLogin from "../src/components/guestlogin";
+import PayButton from "../src/components/paybutton";
+import PickUpButton from "../src/components/pickupbutton";
 
 class App extends Component {
   render() {
     return (
       <LoginCard>
       <LogoButton>logo</LogoButton>
+      <PickUpButton>pickupbutton</PickUpButton>
+      <PayButton>paybutton</PayButton>
       <GuestLogin>guestlogin</GuestLogin>
       <ValetDriverButton>valetdriver</ValetDriverButton>
       </LoginCard>
