bee763261bfac6f9270b3dbc920537a9490947b7
Anthony Gates <setag9697@yahoo.com>
2019-09-14 12:46:13 -0500

    guestloginform2

---
+++ create-react-express/client/src/components/guestloginform/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './style.css';
+
+
+class GuestLoginForm extends React.Component {
+    render() {
+      return (
+        <form>
+        
+          <label for="firstname">First Name</label>
+          <input
+            type="text"
+            id="firstname"         
+            />
+           <label for="lastname">Last Name</label>
+          <input
+            type="text"
+            id="lastname" 
+            />
+            <label for="licenseplate">License Plate</label>
+          <input
+            type="text"
+            id="licenseplate"
+            />
+            {this.props.children}
+        </form>
+
+      );
+    }
+  }
+
+export default GuestLoginForm;
