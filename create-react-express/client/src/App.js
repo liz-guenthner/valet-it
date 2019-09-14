ee20a335fbedca7a3055cb56008fc21770b84ee9
Anthony Gates <setag9697@yahoo.com>
2019-09-14 08:38:13 -0500

    seequeuebutton

--- create-react-express/client/src/App.js
+++ create-react-express/client/src/App.js
@@ -5,12 +5,14 @@ import LogoButton from "../src/components/logoButton";
 import LoginCard from "../src/components/loginCard";
 import ValetDriverButton from "../src/components/valetdriverbutton";
 import GuestLogin from "../src/components/guestlogin";
+import SeeQueueButton from "../src/components/seequeuebutton";
 
 class App extends Component {
   render() {
     return (
       <LoginCard>
       <LogoButton>logo</LogoButton>
+      <SeeQueueButton>seequeue</SeeQueueButton>
       <GuestLogin>guestlogin</GuestLogin>
       <ValetDriverButton>valetdriver</ValetDriverButton>
       </LoginCard>
