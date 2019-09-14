e0c43e5b21a08f773c1cd0046f63957d4fb2181f
Anthony Gates <setag9697@yahoo.com>
2019-09-14 08:31:02 -0500

    addcardbutton

--- create-react-express/client/src/App.js
+++ create-react-express/client/src/App.js
@@ -5,12 +5,14 @@ import LogoButton from "../src/components/logoButton";
 import LoginCard from "../src/components/loginCard";
 import ValetDriverButton from "../src/components/valetdriverbutton";
 import GuestLogin from "../src/components/guestlogin";
+import AddCardButton from "../src/components/addcardbutton";
 
 class App extends Component {
   render() {
     return (
       <LoginCard>
       <LogoButton>logo</LogoButton>
+      <AddCardButton>addcard</AddCardButton>
       <GuestLogin>guestlogin</GuestLogin>
       <ValetDriverButton>valetdriver</ValetDriverButton>
       </LoginCard>
