7c3a74889a4f352d3f1208c5c42a41da0a9a011e
Anthony Gates <setag9697@yahoo.com>
2019-09-14 08:18:59 -0500

    paynowbutton

--- create-react-express/client/src/App.js
+++ create-react-express/client/src/App.js
@@ -5,12 +5,14 @@ import LogoButton from "../src/components/logoButton";
 import LoginCard from "../src/components/loginCard";
 import ValetDriverButton from "../src/components/valetdriverbutton";
 import GuestLogin from "../src/components/guestlogin";
+import PayNowButton from "../src/components/paynowbutton";
 
 class App extends Component {
   render() {
     return (
       <LoginCard>
       <LogoButton>logo</LogoButton>
+      <PayNowButton>paynowbutton</PayNowButton>
       <GuestLogin>guestlogin</GuestLogin>
       <ValetDriverButton>valetdriver</ValetDriverButton>
       </LoginCard>
